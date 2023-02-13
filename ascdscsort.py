d17abf14b67314198a12e08c872de82136655cb4
ARUNDHATHYGK <115702662+ARUNDHATHYGK@users.noreply.github.com>
2023-02-13 15:35:13 +0530

    python

--- ascdscsort.py
+++ ascdscsort.py
@@ -8,5 +8,5 @@ for i in range(0,n):
 print("Dictionary",employees)
 a=sorted(employees.items(),key=operator.itemgetter(1))
 print("Ascending order is",a)
-#d=dict(sorted (employees.item(),key=operator.itemgetter(1),reverse=True))
-#print("Descending order is",d)
+d=dict(sorted (employees.item(),key=operator.itemgetter(1),reverse=True))
+print("Descending order is",d)
